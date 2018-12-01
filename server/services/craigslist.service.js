ee4c5b3e5caa4ff9d9ac8440539a280ab29c7bc9
Dima Mukhin <dima9348@gmail.com>
2018-12-01 13:38:22 -0800

    added /free get and post

--- server/services/craigslist.service.js
+++ server/services/craigslist.service.js
@@ -1,15 +1,17 @@
 const craigslist = require('node-craigslist');
 
+const FREE_CATEGORY_CODE = 'zip';
 const client = new craigslist.Client();
-
 const craigslistService = {};
 
 craigslistService.findFree = async ({
     city,
-    item,
-    email
+    item
 }) => {
-    return await client.search({ city }, item);
+    return await client.search({
+        city,
+        category: FREE_CATEGORY_CODE
+    }, item);
 };
 
 module.exports = craigslistService;
