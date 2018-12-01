57870d0b88045a903937efa50b6e129ea2d2a407
Dima Mukhin <dima9348@gmail.com>
2018-12-01 12:01:48 -0800

    implemented api structure

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// setting json parsing of http requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// enabling cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+// setting /api route as the default api route of the application
+app.use('/api', routes);
+
+// start listening for requests on the given port
+app.listen(port, () => {
+  console.log('Server running on port:' + port);
+});
+
+module.exports = app;
